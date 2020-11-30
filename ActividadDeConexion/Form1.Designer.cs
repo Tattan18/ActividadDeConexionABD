a311d1fc03d54699c94aff57e02465fd1b456758
Sebastian Santander <wsantander@unicesar.edu.co>
2020-11-30 15:19:02 -0500

    commit

---
+++ ActividadDeConexion/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ActividadDeConexion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.ImageIndex = 0;
+            this.btnConectar.ImageList = this.imageList1;
+            this.btnConectar.Location = new System.Drawing.Point(35, 35);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(160, 135);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Probar Conexión";
+            this.btnConectar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnConectar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "search.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 211);
+            this.Controls.Add(this.btnConectar);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(250, 250);
+            this.MinimumSize = new System.Drawing.Size(250, 250);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conexión a BD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
